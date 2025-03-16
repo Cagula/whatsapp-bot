63c4a9c2937611e3eef3cfb51ae71621457724de
haram <164263503+Cagula@users.noreply.github.com>
2025-03-16 01:10:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const QRCode = require('qrcode'); // Asigură-te că această bibliotecă este instalată
+const QRCode = require('qrcode'); // Asigură-te că este instalat
+const path = require('path');
 
 const app = express();
+
+// Middleware pentru procesarea datelor din formulare
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Servește fișierul HTML principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Endpoint pentru generarea codului QR
 app.post('/generate-qr', (req, res) => {
     const { text } = req.body;
 
     if (!text) {
-        return res.status(400).send('Textul pentru generarea QR este obligatoriu!');
+        return res.status(400).send('Textul este obligatoriu pentru generarea QR!');
     }
 
     QRCode.toDataURL(text, (err, url) => {
         if (err) {
-            return res.status(500).send('A apărut o eroare la generarea codului QR.');
+            return res.status(500).send('A apărut o eroare la generarea QR.');
         }
-
         res.send({ qrCodeUrl: url });
     });
 });
