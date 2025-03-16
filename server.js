3c406fa811903da329b4ff08c6e609e135f85900
haram <164263503+Cagula@users.noreply.github.com>
2025-03-16 01:02:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const QRCode = require('qrcode'); // Biblioteca pentru generarea codurilor QR
 
 const app = express();
 
@@ -8,28 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servește fișierul HTML la ruta principală "/"
+// Servește fișierul HTML la ruta principală
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Endpoint pentru generarea codului QR
-app.post('/generate-qr', (req, res) => {
-    const { text } = req.body;
-
-    if (!text) {
-        return res.status(400).send('Te rog să furnizezi textul pentru codul QR!');
-    }
-
-    // Generează codul QR sub formă de URL de imagine
-    QRCode.toDataURL(text, (err, url) => {
-        if (err) {
-            return res.status(500).send('A apărut o eroare la generarea codului QR!');
-        }
-
-        res.send({ qrCodeUrl: url });
-    });
-});
-
 // Pornire server
-const
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server pornit pe portul ${PORT}`);
+});
